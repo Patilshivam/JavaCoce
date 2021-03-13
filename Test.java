919e8768010306d4b530e9bbcd414cf02248ae5f
Patilshivam <35370465+Patilshivam@users.noreply.github.com>
2021-03-13 13:35:05 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 class Test {
   public static void main(String args[]) {
-    System.out.println("Hello Shivam, Welcome to Git and Jankins");
+    System.out.println("Hello Shivam, Welcome to Git and Jankins V2");
   }
 }
